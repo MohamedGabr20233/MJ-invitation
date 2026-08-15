57eefa101138b6b0f29ad5678904e16f18de0254
MohamedGabr20233 <mohamedgbr20233@gmail.com>
2026-08-15 20:41:10 +0300

    fix: adjust layout and positioning of map link and place name in BoatSection

--- src/site/sections/Boat/BoatSection.tsx
+++ src/site/sections/Boat/BoatSection.tsx
@@ -13,21 +13,22 @@ const BoatSection = () => {
       <div className="relative">
         <img src="/boat.jpeg" className=" h-full w-full  object-cover mt-10 [clip-path:polygon(0_10%,100%_0,100%_90%,0_100%)]" alt="boat img" />
         {/* the location badge */}
-        <div className="absolute  top-0 left-0   rotate-10 w-full">
+        <div className="absolute  top-0 left-0    rotate-10 w-full">
           <img src="/map-paper.png" className="w-110   top-0  -translate-x-1/4" alt="location" />
-          <div className="absolute  left-15  top-26 -rotate-1 max-[300px]:top-17 max-[300px]:left-6  max-[340px]:top-22 max-[340px]:left-12  max-[300px]:text-base text-white text-xl font-cormorant font-bold uppercase">
-            <a className=" px-10 py-5 " target="_blank" href={InvitationLocations.link}>
-              {" "}
-              OPEN MAP
-            </a>
-          </div>
+          <a
+            className="absolute  left-17 p-0 w-40 h-20 flex items-center justify-center top-19  -rotate-1 max-[300px]:top-17 max-[300px]:left-6  max-[340px]:top-22 max-[340px]:left-12  max-[300px]:text-base text-white text-xl font-cormorant font-bold uppercase  z-999 "
+            target="_blank"
+            href={InvitationLocations.link}
+          >
+            OPEN MAP
+          </a>
         </div>
 
         {/* the place name patch */}
-        <div className="absolute  top-14 left-40   -rotate-10 w-full">
+        <div className="absolute  top-6  max-md:top-12  left-42 max-md:left-35 [clip-path:polygon(0_28%,100%_28%,100%_100%,0_100%)] px-2  z-10  -rotate-10 w-full">
           {/* the white badge */}
-          <img src="/white-paper.png" className="w-60      -translate-x-1/4" alt="location" />
-          <p className="absolute top-15 font-alex font-bold  text-ink text-lg left-0">Paradise Yacht</p>
+          <img src="/white-paper.png" className="w-60       -translate-x-1/4" alt="location" />
+          <p className="absolute top-15 font-alex font-bold  text-ink text-lg left-2">Paradise Yacht</p>
         </div>
       </div>
 
