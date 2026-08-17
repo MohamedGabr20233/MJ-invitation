3147041269dc3d38f8218ff5f82e1d8142baa007
MohamedGabr20233 <mohamedgbr20233@gmail.com>
2026-08-17 19:38:13 +0300

    fix the value issue

--- src/site/sections/Game/GameCard.tsx
+++ src/site/sections/Game/GameCard.tsx
@@ -2,23 +2,24 @@ import { useState } from "react";
 
 const GameCard = ({ className, backImage, alt, value }: { className?: string; backImage: string; alt: string; value: boolean }) => {
   const [flipped, setFlipped] = useState(false);
+  console.log(value);
   return (
     // the card container
     <div onClick={() => setFlipped((prev) => !prev)} className={className || "relative rounded-xl w-1/3 h-60 min-w-45 perspective-distant"}>
       {/* the card */}
       <div
         className={`relative h-60 w-full cursor-pointer transition-transform
-        duration-700 transform-3d ${flipped ? "transform-[rotateY(180deg)]" : ""}
+        duration-700 transform-3d will-change-transform ${flipped ? "transform-[rotateY(180deg)]" : ""}
         `}
       >
         {/* the front */}
-        <div className="absolute inset-0 overflow-hidden backface-hidden rounded-xl">
-          <img src="/rapunzel-play-card.png" alt="play card" className="h-60 w-full object-cover" />
+        <div className="absolute inset-0 backface-hidden">
+          <img src="/rapunzel-play-card.png" alt="play card" width={1024} height={1536} className="h-60 w-full rounded-xl object-cover" />
         </div>
 
         {/* the back */}
-        <div className="absolute inset-0 overflow-hidden backface-hidden transform-[rotateY(180deg)] rounded-xl">
-          <img className="h-60 w-full object-cover" alt={alt} src={backImage} />
+        <div className="absolute inset-0 backface-hidden transform-[rotateY(180deg)]">
+          <img className="h-60 w-full rounded-xl object-cover" alt={alt} src={backImage} />
         </div>
       </div>
     </div>
